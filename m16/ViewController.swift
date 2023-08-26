06d0701bf63811c4f109d78a615756aa543b18a2
Viktor Nazarov <github.com/NazarVik>
2023-08-26 11:48:57 +0300

    first commit

---
+++ m16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  m16
+//
+//  Created by Виктор Назаров on 26.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("First commit")
+    }
+
+
+}
+
